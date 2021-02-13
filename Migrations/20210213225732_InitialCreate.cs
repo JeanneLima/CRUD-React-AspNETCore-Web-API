324ff7b22ad68baab232cefb4ad3ac0f9cff998d
Jeanne Lima <jeannelima.profissional@gmail.com>
2021-02-13 20:03:00 -0300

    Add project files.

---
+++ Migrations/20210213225732_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD.Web.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidate",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fullName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    mobile = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(6)", nullable: true),
+                    age = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    bloodGroup = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    address = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidate", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Candidate",
+                columns: new[] { "id", "address", "age", "bloodGroup", "email", "fullName", "mobile" },
+                values: new object[] { 1, "Rua do Expedicionário, 12, São Cristóvão - RJ, Rio de Janeiro", "47", "O+", "rose.manhaes@gmail.com", "Rose Manhães", "Lorem ipsum" });
+
+            migrationBuilder.InsertData(
+                table: "Candidate",
+                columns: new[] { "id", "address", "age", "bloodGroup", "email", "fullName", "mobile" },
+                values: new object[] { 2, "Rua Dias da Cruz, 106, Méier - RJ, Rio de Janeiro", "28", "AB+", "gustavo.lima@gmail.com", "Gustavo Lima", "Lorem ipsum" });
+
+            migrationBuilder.InsertData(
+                table: "Candidate",
+                columns: new[] { "id", "address", "age", "bloodGroup", "email", "fullName", "mobile" },
+                values: new object[] { 3, "Rua Santa Fé, 960, Inhaúma - RJ, Rio de Janeiro", "34", "A+", "claudia.martins@gmail.com", "Claudia Martins", "Lorem ipsum" });
+
+            migrationBuilder.InsertData(
+                table: "Candidate",
+                columns: new[] { "id", "address", "age", "bloodGroup", "email", "fullName", "mobile" },
+                values: new object[] { 4, "Rua Ernani Agricola, 73, Itanhangá - RJ, Rio de Janeiro", "74", "O-", "jose.goncalves@gmail.com", "José Gonçalves", "Lorem ipsum" });
+
+            migrationBuilder.InsertData(
+                table: "Candidate",
+                columns: new[] { "id", "address", "age", "bloodGroup", "email", "fullName", "mobile" },
+                values: new object[] { 5, "Travessa Ricardo Barbosa, 15, Freguesia (Jacarepaguá) - RJ, Rio de Janeiro", "18", "B+", "pedro.lucas@gmail.com", "Pedro Lucas", "Lorem ipsum" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidate");
+        }
+    }
+}
---
+++ Models/DonationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Web.API.Models
+{
+    public class DonationDbContext:DbContext
+    {
+        public DonationDbContext(DbContextOptions<DonationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Candidate> Candidate { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Candidate>().HasData(
+                new Candidate { id = 1, fullName = "Rose Manhães", mobile = "Lorem ipsum", email = "rose.manhaes@gmail.com", age = "47", bloodGroup = "O+", address = "Rua do Expedicionário, 12, São Cristóvão - RJ, Rio de Janeiro" },
+                new Candidate { id = 2, fullName = "Gustavo Lima", mobile = "Lorem ipsum", email = "gustavo.lima@gmail.com", age = "28", bloodGroup = "AB+", address = "Rua Dias da Cruz, 106, Méier - RJ, Rio de Janeiro" },
+                new Candidate { id = 3, fullName = "Claudia Martins", mobile = "Lorem ipsum", email = "claudia.martins@gmail.com", age = "34", bloodGroup = "A+", address = "Rua Santa Fé, 960, Inhaúma - RJ, Rio de Janeiro" },
+                new Candidate { id = 4, fullName = "José Gonçalves", mobile = "Lorem ipsum", email = "jose.goncalves@gmail.com", age = "74", bloodGroup = "O-", address = "Rua Ernani Agricola, 73, Itanhangá - RJ, Rio de Janeiro" },
+                new Candidate { id = 5, fullName = "Pedro Lucas", mobile = "Lorem ipsum", email = "pedro.lucas@gmail.com", age = "18", bloodGroup = "B+", address = "Travessa Ricardo Barbosa, 15, Freguesia (Jacarepaguá) - RJ, Rio de Janeiro" }
+            );
+        }
+    }
+}
