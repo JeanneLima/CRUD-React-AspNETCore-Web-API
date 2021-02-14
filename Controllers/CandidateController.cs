42dcc1fcbfdc42aeb0e60ec2c4f21d3a46f5ac00
Jeanne Lima <jeannelima.profissional@gmail.com>
2021-02-13 22:32:02 -0300

    Adiciona controller de candidate com os métodos HTTP (GET, PUT, POST, DELETE)

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CRUD.Web.API.Models;
+
+namespace CRUD.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly DonationDbContext _context;
+
+        public CandidateController(DonationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Candidate
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Candidate>>> GetCandidate()
+        {
+            return await _context.Candidate.ToListAsync();
+        }
+
+        // GET: api/Candidate/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Candidate>> GetCandidate(int id)
+        {
+            var candidate = await _context.Candidate.FindAsync(id);
+
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            return candidate;
+        }
+
+        // PUT: api/Candidate/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCandidate(int id, Candidate candidate)
+        {
+
+            candidate.id = id;
+           
+            _context.Entry(candidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Candidate
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Candidate>> PostCandidate(Candidate candidate)
+        {
+            _context.Candidate.Add(candidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCandidate", new { id = candidate.id }, candidate);
+        }
+
+        // DELETE: api/Candidate/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCandidate(int id)
+        {
+            var candidate = await _context.Candidate.FindAsync(id);
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Candidate.Remove(candidate);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CandidateExists(int id)
+        {
+            return _context.Candidate.Any(e => e.id == id);
+        }
+    }
+}
